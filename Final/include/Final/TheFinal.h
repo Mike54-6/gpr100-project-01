cb5cf32d36b2017782cfdc68a8e38b0ef479fe71
michael.hills <michael.hills@CHAMPLAIN.EDU>
2020-02-13 20:14:07 -0500

    associated basics of TTT w/ appropriate files

--- Final/include/Final/TheFinal.h
+++ Final/include/Final/TheFinal.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef _THEFINAL_H_
+#define _THEFINAL_H_
+
+//display functions
+void drawGrid(int width, int height);
+void quitPrompt();
+void showWinner(int player);
+
+//input functions
+int p1pickX();
+int p1pickY();
+int p2pickX();
+int p2pickY();
+void quitGame();
+
+#endif //_THEFINAL_H_
---
+++ Final/source/Final/TheFinal.cpp
@@ -0,0 +1,73 @@
+#include "Final/TheFinal.h"
+
+using namespace std;
+#include <iostream>
+
+void drawGrid(int width, int height)
+{
+
+	cout << "Board is " << height << " by " << width << ". Please select your coordinate" << endl;
+	for (int i = 0; i < height; i++)
+	{
+		cout << endl;
+		for (int j = 0; j < width; j++)
+		{
+			cout << ".";
+		}
+	}
+	cout << endl;
+}
+
+void quitPrompt()
+{
+	cout << "Type 100 at any time to quit" << endl;
+}
+
+void showWinner(int player)
+{
+	if (player == 1)
+	{
+		cout << "Player one wins!";
+	}
+	else if (player == 2)
+	{
+		cout << "Player two wins!";
+	}
+}
+
+int p1pickX()
+{
+	int x;
+	cout << "Player 1 pick x coord: ";
+	cin >> x;
+	return x;
+}
+
+int p1pickY()
+{
+	int y;
+	cout << "Player 1 pick y coord: ";
+	cin >> y;
+	return y;
+}
+
+int p2pickX()
+{
+	int x;
+	cout << "Player 2 pick x coord: ";
+	cin >> x;
+	return x;
+}
+
+int p2pickY()
+{
+	int y;
+	cout << "Player 2 pick y coord: ";
+	cin >> y;
+	return y;
+}
+
+void quitGame()
+{
+	exit(0);
+}
